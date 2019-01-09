c9e65466e13be052870d84dcdcb4791d773d6013
EreboPSilva <ereboperezsilva@gmail.com>
2019-01-09 17:40:38 +0100

    Actualizacion

--- scripts/md5checker_mac.sh
+++ scripts/md5checker_mac.sh
@@ -2,9 +2,12 @@
 
 for i in $(ls *.gz);
 do
-        md5gz=$(md5 $i | cut -d " " -f1)
+        md5gz=$(md5 $i | cut -d " " -f4)
         md5su=$(cat $i.md5 | cut -d " " -f1)
 
+        echo $md5gz
+        echo $md5su
+        
         if  [ "$md5gz" = "$md5su" ]
         then
                 echo "$i IS OK!"
